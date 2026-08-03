8672f7ffdc5871c3943fd4c23e5202aa2c738be5
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-08-03 16:49:17 +0200

    Hourly forecast/screen 2 styling done with glossy border on 'Now' window

--- src/components/Container-Left/WeatherInfo.tsx
+++ src/components/Container-Left/WeatherInfo.tsx
@@ -1,18 +1,20 @@
 import { Text } from '../../components/Text'
 import darkClouds from '../../assets/dark-clouds.jpg'
+import React, { useState } from 'react'
 
 export const WeatherInfo = () => {
+
     return (
         <div className='main-weather-screen'>
             <div className='screen-1'>
                 <img src={darkClouds} alt='Dark Clouds Background' id='dark-clouds-bg' />
                 <div className='screen-1-elements'>
                     <div className='left-elements'>
-                        
+
                         <Text variant='h1' style={{ color: 'white' }} className={'screen-1-text'}>18&#176;</Text>
                         <Text variant='h2' style={{ color: 'white' }} className={'screen-1-text'}>Stormy</Text>
                         <Text variant='h3' style={{ marginTop: '-15px' }} className={'screen-1-text'}>with partly cloudy</Text>
-                        
+
                         <div className='high-low'>
                             <div id='high'>
                                 <Text variant='h3' className={'screen-1-text'}>H</Text>
@@ -31,10 +33,55 @@ export const WeatherInfo = () => {
                 </div>
             </div>
             <div className='screen-2'>
-
+                <div className='now-hourly-forecast'>
+                    <Text variant='p'>Now</Text>
+                    <img src={'cloudy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>18&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>2 PM</Text>
+                    <img src={'cloudy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>19&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>3 PM</Text>
+                    <img src={'cloudy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>20&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>4 PM</Text>
+                    <Text variant='p' className='rain-chance'>60%</Text>
+                    <img src={'rainy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>20&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>5 PM</Text>
+                    <Text variant='p' className='rain-chance'>60%</Text>
+                    <img src={'rainy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>19&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>6 PM</Text>
+                    <img src={'cloudy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>18&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>7 PM</Text>
+                    <img src={'cloudy.png'} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>17&#176;</Text>
+                </div>
+                <div className='hourly-forecast'>
+                    <Text variant='p'>8 PM</Text>
+                    <img src={'night.png'} style={{ filter: 'invert(83%)' }} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>16&#176;</Text>
+                </div>
             </div>
             <div className='screen-3'>
-
+                <div className='hourly-forecast-2'>
+                    <Text variant='p'>8 PM</Text>
+                    <img src={'night.png'} style={{ filter: 'invert(83%)' }} alt='Cloudy Weather Icon' id='cloudy-img' />
+                    <Text variant='p'>16&#176;</Text>
+                </div>
             </div>
         </div>
     )
--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import pinIcon from '../../assets/pin.png'
 import { Text } from '../../components/Text'
 
@@ -6,6 +6,8 @@ export const Navbar = () => {
 
     const downloadURL = "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://apps.apple.com/us/app/weather/id1069513131&ved=2ahUKEwjzyI_ZgP2VAxVZzwIHHZg6E0gQFnoECCAQAQ&usg=AOvVaw23Q0okt8s3y18BrV6CAjPh"
     const [showSearch, toggleSearch] = useState(false);
+    const [locations, setLocations] = useState(false)
+
     return (
         <div className='nav-main'>
             <div className='left-nav-content'>
@@ -18,8 +20,12 @@ export const Navbar = () => {
 
             <div className='right-nav-content'>
                 <div className='search-section'>
-                    <img src={'search.png'} alt='Search Icon' id='nav-search-icon' />
-                    <input type='text' className='search-bar' placeholder='Search City' />
+                    <img src={'search.png'} alt='Search Icon' id='nav-search-icon' onClick={() => toggleSearch(!showSearch)} />
+
+                    {showSearch && (
+                        <input type='text' className='search-bar' placeholder='Search City' />
+                    )}
+
                 </div>
                 <a href={downloadURL} target='_blank' rel='noopener noreferrer' id='download-app-btn'>Download App</a>
             </div>
